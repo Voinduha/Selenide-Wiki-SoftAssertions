78d747f6dff05b96e106a1f3fb44df52d8d87341
DanVu <danvu@ya.ru>
2021-02-05 19:46:58 +0300

    added SoftAssertionPage

--- src/test/java/SoftAssertionTest.java
+++ src/test/java/SoftAssertionTest.java
@@ -3,26 +3,27 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionTest {
 
     @Test
     void MustFindSelenideText() {
         // open GitHub
-        Selenide.open("https://github.com/");
+        open("https://github.com/");
         // ввести в поле поиска Selenide and press Enter
-        Selenide.$("[name=q]").setValue("Selenide").pressEnter();
+        $("[name=q]").setValue("Selenide").pressEnter();
         // выбрать первый найденный проект из списка
-        Selenide.$("ul.repo-list li a").click();
+        $("ul.repo-list li a").click();
         // проверка в заголовке встречается selenide / selenide
-        Selenide.$("h1").shouldHave(text("selenide / selenide"));
+        $("h1").shouldHave(text("selenide / selenide"));
         // переходим в раздел Wiki проекта
-        Selenide.$("[data-content='Wiki']").click();
+        $("[data-content='Wiki']").click();
         // убеждаемся что он видимый
-        Selenide.$("#wiki-body div ul").shouldBe(Condition.visible);
+        $("#wiki-body div ul").shouldBe(Condition.visible);
         // ищем текст
-        Selenide.$$("#wiki-pages-box li").findBy(text("SoftAssertions")).click();
+        $$("#wiki-pages-box li").findBy(text("SoftAssertions")).click();
         // убеждаемся что
-        Selenide.$("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
 }
